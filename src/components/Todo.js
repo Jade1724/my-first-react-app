813fc708d075f34c71f954fa07c0806b1b1b699a
Haruka Ichinose <hic21@uclive.ac.nz>
2022-04-25 23:00:11 +1200

    Added state to toggle showing Modal and BackDrop.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,33 @@
+import { useState } from "react";
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 function Todo(props) {
+  const [modalIsOpen, setModalIsOpen ] = useState(false);
 
   function deleteHandler() {
-    console.log('Clicked!');
-    console.log(props.text);
+    setModalIsOpen(true);
+  }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
   }
   return (
     <div className="card">
-      <h2>{ props.text }</h2>
+      <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn" onClick={ deleteHandler }>Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
+      {modalIsOpen && <Modal onConfirm={ closeModalHandler } onCancel={ closeModalHandler }/>}
+      {modalIsOpen && <Backdrop onCancel={ closeModalHandler }/>}
     </div>
   );
 }
 
 export default Todo;
+
+const props = {
+  text: 'Learn React'
+}
